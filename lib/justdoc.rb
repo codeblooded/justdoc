57bc657ccdec1970974190cb47a3aaf367f086d3
Benjamin Reed <benvreed@gmail.com>
2013-03-21 23:05:48 -0500

    Add Logger

--- lib/justdoc.rb
+++ lib/justdoc.rb
@@ -7,18 +7,31 @@
 
 require "justdoc/version"
 require "justdoc/tracker"
+require "justdoc/setup"
 require "justdoc/generator"
 require "justdoc/reader"
 require "justdoc/recognizer"
 require "justdoc/outputs/markdown"
 
 module Justdoc
-  def run_with_git
+  
+  def self.prepare_log
+    # set the logger to be enabled if it is verbose
+    enable_verbose = $options[:verbose] || false
+    $vl = Logger.new enabled: enable_verbose
+  end 
+  
+  def self.run_setup
+    prepare_log
     Setup.create_directory_and_config
     Setup.add_to_gitignore
   end
   
-  def run_with_file(data)
+  def self.run_with_git
+    
+  end
+  
+  def self.run_with_file(data)
     data = File.expand_path(data, Dir.pwd)
     reader = Justdoc::Reader.new(data)
     reader.read_and_recognize
--- lib/justdoc/setup.rb
+++ lib/justdoc/setup.rb
@@ -5,19 +5,23 @@ require 'yaml'
 
 module Justdoc
   class Setup
-    class << self
-    def create_directory_and_config
-      base = {version: Justdoc::VERSION, created_at: Time.now.utc}
+    
+    def self.create_directory_and_config
+      
+      base = {version: Justdoc::VERSION, created_at: Time.now}
       Dir.mkdir(".docs")
+      $vl.la action: "create" text: ".docs/"
       File.open(".docs/.justdoc.yml", "w+") do |f|
         f.write(base.to_yaml)
       end
+      $vl.la action: "create" text: ".docs/justdoc.yml"
     end
     
-    def add_to_gitignore
+    def self.add_to_gitignore
       File.open(".gitignore", "a") do |f|
         f.puts ".docs/*"
       end
     end
+    
   end
 end
